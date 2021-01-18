8c68f8ecda0f12cfa9279cbf04eb69daf8c6f6ff
Christina Asquith <christinaasquith@gmail.com>
2021-01-18 06:38:15 -0500

    add js so to change color with a click

---
+++ script.js
@@ -0,0 +1,8 @@
+const part = document.querySelector('#part')
+part.addEventListener('click', function() {
+   console.log ('div part is changed') 
+   part.style.backgroundColor = 'purple'
+   //part.isclicked.add('part.isclicked')
+
+})
+
